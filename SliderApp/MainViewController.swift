ef999743749369ce19f8acd8e325d26a03f054de
Rose Khaldeeva <khalrose@mail.ru>
2022-11-06 17:40:14 +0600

    Add and set up MainViewController

---
+++ SliderApp/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SliderApp
+//
+//  Created by Rose on 04.11.2022.
+//
+
+import UIKit
+
+protocol ColorViewControllerDelegate {
+    func setColor(_ color: UIColor)
+}
+
+class MainViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let colorVC = segue.destination as! ColorViewController
+        colorVC.delegate = self
+        colorVC.colorView = view.backgroundColor
+    }
+}
+
+extension MainViewController: ColorViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
+    
+
